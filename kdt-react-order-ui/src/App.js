3af60ca6058fc23820c8bcb2b2d0561747dab10a
H2W0N <woohw97@naver.com>
2022-10-20 00:12:54 +0900

    feat : react로 화면구성 완료

--- kdt-react-order-ui/src/App.js
+++ kdt-react-order-ui/src/App.js
@@ -1,85 +1,73 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import React, {useEffect, useState} from 'react';
+import {ProductList} from "./components/ProductList";
+import {Summary} from "./components/Summary";
+import axios from "axios";
 
 function App() {
-  return (
-      <div className="container-fluid">
-      <div className="row justify-content-center m-4">
-        <h1 className="text-center">Grids & Circle</h1>
-      </div>
-      <div className="card">
-        <div className="row">
-          <div className="col-md-8 mt-4 d-flex flex-column align-items-start p-3 pt-0">
-            <h5 className="flex-grow-0"><b>상품 목록</b></h5>
-            <ul className="list-group products">
-              <li className="list-group-item d-flex mt-3">
-                <div className="col-2"><img className="img-fluid" src="https://i.imgur.com/HKOFQYa.jpeg" alt=""/></div>
-                <div className="col">
-                  <div className="row text-muted">커피콩</div>
-                  <div className="row">Columbia Nariñó</div>
-                </div>
-                <div className="col text-center price">5000원</div>
-                <div className="col text-end action"><a className="btn btn-small btn-outline-dark" href="">추가</a></div>
-              </li>
-              <li className="list-group-item d-flex mt-2">
-                <div className="col-2"><img className="img-fluid" src="https://i.imgur.com/HKOFQYa.jpeg" alt=""/></div>
-                <div className="col">
-                  <div className="row text-muted">커피콩</div>
-                  <div className="row">Columbia Nariñó</div>
-                </div>
-                <div className="col text-center price">5000원</div>
-                <div className="col text-end action"><a className="btn btn-small btn-outline-dark" href="">추가</a></div>
-              </li>
-              <li className="list-group-item d-flex mt-2">
-                <div className="col-2"><img className="img-fluid" src="https://i.imgur.com/HKOFQYa.jpeg" alt=""/></div>
-                <div className="col">
-                  <div className="row text-muted">커피콩</div>
-                  <div className="row">Columbia Nariñó</div>
-                </div>
-                <div className="col text-center price">5000원</div>
-                <div className="col text-end action"><a className="btn btn-small btn-outline-dark" href="">추가</a></div>
-              </li>
-            </ul>
-          </div>
-          <div className="col-md-4 summary p-4">
-            <div>
-              <h5 className="m-0 p-0"><b>Summary</b></h5>
+    const [products, setProducts] = useState([
+        {productId: 'uuid-1', productName: '콜롬비아 커피 1', category: '커피빈', price: 5000},
+        {productId: 'uuid-2', productName: '콜롬비아 커피 2', category: '커피빈', price: 5000},
+        {productId: 'uuid-3', productName: '콜롬비아 커피 3', category: '커피빈', price: 5000},
+    ]);
+    const [items, setItems] = useState([]);
+    const handleAddClicked = productId => {
+        const product = products.find(v => v.productId === productId);
+        const found = items.find(v => v.productId === productId);
+        const updatedItems =
+            found ? items.map(v => (v.productId === productId) ? {...v, count: v.count + 1} : v) : [...items, {
+                ...product,
+                count: 1
+            }]
+        setItems(updatedItems);
+    }
+
+    useEffect(() => {
+        axios.get('http://localhost:8080/api/v1/products')
+            .then(v => setProducts(v.data));
+    }, []);
+
+    const handleOrderSubmit = (order) => {
+        if (items.length === 0) {
+            alert("아이템을 추가해 주세요!");
+        } else {
+            axios.post('http://localhost:8080/api/v1/orders', {
+                email: order.email,
+                address: order.address,
+                postcode: order.postcode,
+                orderItems: items.map(v => ({
+                    productId: v.productId,
+                    category: v.category,
+                    price: v.price,
+                    quantity: v.count
+                }))
+            }).then(
+                v => alert("주문이 정상적으로 접수되었습니다."),
+                e => {
+                    alert("서버 장애");
+                    console.error(e);
+                })
+        }
+    }
+
+    return (
+        <div className="container-fluid">
+            <div className="row justify-content-center m-4">
+                <h1 className="text-center">Grids & Circle</h1>
             </div>
-            <hr/>
-              <div className="row">
-                <h6 className="p-0">Columbia Nariñó <span className="badge bg-dark text-">2개</span></h6>
-              </div>
-              <div className="row">
-                <h6 className="p-0">Brazil Serra Do Caparaó <span className="badge bg-dark">2개</span></h6>
-              </div>
-              <div className="row">
-                <h6 className="p-0">Columbia Nariñó <span className="badge bg-dark">2개</span></h6>
-              </div>
-              <form>
-                <div className="mb-3">
-                  <label htmlFor="email" className="form-label">이메일</label>
-                  <input type="email" className="form-control mb-1" id="email"/>
+            <div className="card">
+                <div className="row">
+                    <div className="col-md-8 mt-4 d-flex flex-column align-items-start p-3 pt-0">
+                        <ProductList products={products} onAddClick={handleAddClicked}/>
+                    </div>
+                    <div className="col-md-4 summary p-4">
+                        <Summary items={items} onOrderSubmit={handleOrderSubmit}/>
+                    </div>
                 </div>
-                <div className="mb-3">
-                  <label htmlFor="address" className="form-label">주소</label>
-                  <input type="text" className="form-control mb-1" id="address"/>
-                </div>
-                <div className="mb-3">
-                  <label htmlFor="postcode" className="form-label">우편번호</label>
-                  <input type="text" className="form-control" id="postcode"/>
-                </div>
-                <div>당일 오후 2시 이후의 주문은 다음날 배송을 시작합니다.</div>
-              </form>
-              <div className="row pt-2 pb-2 border-top">
-                <h5 className="col">총금액</h5>
-                <h5 className="col text-end">15000원</h5>
-              </div>
-              <button className="btn btn-dark col-12">결제하기</button>
-          </div>
+            </div>
         </div>
-      </div>
-      </div>
-  );
+    );
 }
 
 export default App;

f3ea4a88a35e4270f371f9fcaed60714d46a446a
yeoooo <ruisya330@gmail.com>
2022-10-21 22:34:43 +0900

    feat: cloned order domain and orderStatus

--- src/main/java/com/example/gccoffee/model/Order.java
+++ src/main/java/com/example/gccoffee/model/Order.java
@@ -7,7 +7,6 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
-@RequiredArgsConstructor
 @Getter
 public class Order {
     private final UUID orderId;
@@ -18,6 +17,49 @@ public class Order {
     private OrderStatus orderStatus;
     private final LocalDateTime createdAt;
     private LocalDateTime updatedAt;
+
+    public Order(UUID orderId, Email email, String address, String postcode, List<OrderItem> orderItems, OrderStatus orderStatus, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.orderId = orderId;
+        this.email = email;
+        this.address = address;
+        this.postcode = postcode;
+        this.orderItems = orderItems;
+        this.orderStatus = orderStatus;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public UUID getOrderId() {
+        return orderId;
+    }
+
+    public Email getEmail() {
+        return email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public List<OrderItem> getOrderItems() {
+        return orderItems;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return orderStatus;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
 
     public void setAddress(String address) {
         this.address = address;

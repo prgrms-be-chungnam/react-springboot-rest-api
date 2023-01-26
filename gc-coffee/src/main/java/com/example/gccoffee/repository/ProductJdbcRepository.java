3c45601a4f40a0eead94d3be988269f11e952252
hyunjunKWON <bonheur9813@naver.com>
2023-01-26 17:18:11 +0900

    CloneCoding - January 26

--- gc-coffee/src/main/java/com/example/gccoffee/repository/ProductJdbcRepository.java
+++ gc-coffee/src/main/java/com/example/gccoffee/repository/ProductJdbcRepository.java
@@ -4,12 +4,13 @@ import com.example.gccoffee.model.Category;
 import com.example.gccoffee.model.Product;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
 
-import java.nio.ByteBuffer;
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
+import java.util.*;
 
+import static com.example.gccoffee.JdbcUtils.*;
+
+@Repository
 public class ProductJdbcRepository implements ProductRepository {
     private final NamedParameterJdbcTemplate jdbcTemplate;
 
@@ -24,7 +25,14 @@ public class ProductJdbcRepository implements ProductRepository {
 
     @Override
     public Product insert(Product product) {
-        return null;
+        var update = jdbcTemplate.update("INSERT INTO products(product_id, product_name, category, price, description, created_at, updated_at)" +
+                " VALUES(UUID_TO_BIN(:productId) :productName :category :price :description :createdAt :updatedAt)", toParaMap(product));
+//                " VALUES(UNHEX(REPLACE('77dea2ad-3c8c-40c6-a278-7cf1a1ac9384', '-', '')), ?, ?, ?, ?, ?, ?)", toParaMap(product));
+
+        if (update != 1) {
+            throw new RuntimeException("Nothing was inserted");
+        }
+        return product;
     }
 
     @Override
@@ -54,12 +62,24 @@ public class ProductJdbcRepository implements ProductRepository {
 
     private static final RowMapper<Product> productRowMapper = (rs, rowNum) -> {
         var productId = toUUID(rs.getBytes("product_id"));
-        var productName = rs.getString();
-        return new Product();
-    }
+        var productName = rs.getString("product_name");
+        var category = Category.valueOf(rs.getString("category"));
+        var price = rs.getLong("price");
+        var description = rs.getString("description");
+        var createdAt = toLocalDateTime(rs.getTimestamp("created_at"));
+        var updatedAt = toLocalDateTime(rs.getTimestamp("updated_at"));
+        return new Product(productId, productName, category, price, description, createdAt, updatedAt);
+    };
 
-    static UUID toUUID(byte[] bytes){
-        var byteBuffer = ByteBuffer.wrap(bytes);
-        return new UUID(byteBuffer.getLong(), byteBuffer.getLong());
+    private Map<String, Object> toParaMap(Product product) {
+        var paramMap = new HashMap<String, Object>();
+        paramMap.put("productId", product.getProductId().toString().getBytes());
+        paramMap.put("productName", product.getProductName());
+        paramMap.put("category", product.getCategory().toString());
+        paramMap.put("price", product.getPrice());
+        paramMap.put("description", product.getDescription());
+        paramMap.put("createdAt", product.getCreatedAt());
+        paramMap.put("updatedAt", product.getUpdatedAt());
+        return paramMap;
     }
 }

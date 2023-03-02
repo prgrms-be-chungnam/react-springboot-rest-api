17664c4154cffa6a616c8ae8c75ffb96101d61f6
hagisilm <catchwall3203@naver.com>
2023-03-02 12:15:18 +0900

    feat: 상품 추가,조회,수정,삭제

---
+++ server/src/main/java/com/example/server/repository/ProductRepository.java
@@ -0,0 +1,25 @@
+package com.example.server.repository;
+
+import com.example.server.model.Category;
+import com.example.server.model.Product;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface ProductRepository {
+
+    List<Product> findAll();
+
+    Product insert(Product product);
+
+    Product update(Product product);
+
+    Optional<Product> findById(UUID productId);
+
+    Optional<Product> findByName(String productName);
+
+    List<Product> findByCategory(Category category);
+
+    void deleteById(UUID uuid);
+}

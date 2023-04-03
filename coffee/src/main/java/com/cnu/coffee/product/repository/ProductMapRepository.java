a670e978be89df6971ba933ea439f91645536cfe
ChacunKim <cocodata12@gmail.com>
2023-04-03 17:16:29 +0900

    docs: 주석 삭제

--- coffee/src/main/java/com/cnu/coffee/product/repository/ProductMapRepository.java
+++ coffee/src/main/java/com/cnu/coffee/product/repository/ProductMapRepository.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
-//@Repository
+
 public class ProductMapRepository implements ProductRepository {
 
     private final Map<UUID, Product> db = new HashMap<>();

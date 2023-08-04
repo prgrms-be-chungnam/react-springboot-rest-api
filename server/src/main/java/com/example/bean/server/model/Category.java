e240963d6c70ba70478a0fc52db2c39d3e2112d8
Ji Myoung Ha <webmaster@null0xff.com>
2023-08-04 11:10:59 +0900

    :sparkles: created Category enum and Product class

---
+++ server/src/main/java/com/example/bean/server/model/Category.java
@@ -0,0 +1,13 @@
+package com.example.bean.server.model;
+
+/**
+ * The {@code Category} enum represents various product categories for an online store. Each constant
+ * in this enum represents a unique product category along with a description. Use these constants
+ * to categorize products and perform related operations.
+ *
+ * @version 1.0
+ * @since 2023-08-04
+ */
+public enum Category {
+  COFFEE_BEAN_PACKAGE
+}
---
+++ server/src/main/java/com/example/bean/server/model/Product.java
@@ -0,0 +1,110 @@
+package com.example.bean.server.model;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+import lombok.Getter;
+
+/**
+ * The {@code Product} class represents a product entity with its associated attributes, such as
+ * product ID, name, category, price, description, creation timestamp, and last update timestamp. It
+ * is intended to be used as a data model for products in an online store system where various types
+ * of products need to be managed and stored.
+ *
+ * @version 1.0
+ * @since 2023-08-04
+ */
+
+@Getter
+public class Product {
+
+  private final UUID productID;
+  private final LocalDateTime createdAt;
+  private String productName;
+  private Category category;
+  private long price;
+  private String description;
+  private LocalDateTime updatedAt;
+
+  /**
+   * Constructs a new product with the specified parameters, setting the createdAt and updatedAt
+   * timestamps to the current date and time.
+   *
+   * @param productID   The unique identifier for the product.
+   * @param productName The name of the product.
+   * @param category    The category of the product.
+   * @param price       The price of the product in the smallest unit of currency (e.g., cents).
+   */
+  public Product(UUID productID, String productName, Category category, long price) {
+    this(productID, productName, category, price, "", LocalDateTime.now(), LocalDateTime.now());
+  }
+
+  /**
+   * Constructs a new product with the specified parameters.
+   *
+   * @param productID   The unique identifier for the product.
+   * @param productName The name of the product.
+   * @param category    The category of the product.
+   * @param price       The price of the product in the smallest unit of currency (e.g., cents).
+   * @param description A brief description of the product.
+   * @param createdAt   The timestamp when the product was created.
+   * @param updatedAt   The timestamp when the product was last updated.
+   */
+  public Product(UUID productID, String productName, Category category, long price,
+      String description, LocalDateTime createdAt, LocalDateTime updatedAt) {
+    this.productID = productID;
+    this.productName = productName;
+    this.category = category;
+    this.price = price;
+    this.description = description;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
+  }
+
+  /**
+   * Sets the name of the product and updates the updatedAt timestamp.
+   *
+   * @param productName The new name of the product.
+   */
+  public void setProductName(String productName) {
+    this.productName = productName;
+    update();
+  }
+
+  /**
+   * Sets the category of the product and updates the updatedAt timestamp.
+   *
+   * @param category The new category of the product.
+   */
+  public void setCategory(Category category) {
+    this.category = category;
+    update();
+  }
+
+  /**
+   * Sets the price of the product and updates the updatedAt timestamp.
+   *
+   * @param price The new price of the product in the smallest unit of currency (e.g., cents).
+   */
+  public void setPrice(long price) {
+    this.price = price;
+    update();
+  }
+
+  /**
+   * Sets the description of the product and updates the updatedAt timestamp.
+   *
+   * @param description The new description of the product.
+   */
+  public void setDescription(String description) {
+    this.description = description;
+    update();
+  }
+
+  /**
+   * Updates the updatedAt timestamp to the current date and time. This method is called internally
+   * whenever any attribute of the product is modified.
+   */
+  private void update() {
+    this.updatedAt = LocalDateTime.now();
+  }
+}

243115495d3e79429b03a083b84f5a591e5b8d97
Ji Myoung Ha <webmaster@null0xff.com>
2023-08-04 11:20:09 +0900

    :memo: updated javadoc

--- server/src/main/java/com/example/bean/server/model/Category.java
+++ server/src/main/java/com/example/bean/server/model/Category.java
@@ -1,13 +1,20 @@
 package com.example.bean.server.model;
 
 /**
- * The {@code Category} enum represents various product categories for an online store. Each constant
- * in this enum represents a unique product category along with a description. Use these constants
- * to categorize products and perform related operations.
+ * The {@code Category} enum represents various product categories for an online store. Each
+ * constant in this enum represents a unique product category along with a description. Use these
+ * constants to categorize products and perform related operations.
+ *
+ * <p>This enum provides a structured way to represent and manage product categories. In the
+ * future, more categories can be added to accommodate the expansion of the product range.
  *
  * @version 1.0
  * @since 2023-08-04
  */
 public enum Category {
+  /**
+   * Represents the "Coffee Bean Package" product category. This category is used to categorize
+   * products related to coffee bean packages.
+   */
   COFFEE_BEAN_PACKAGE
 }
--- server/src/main/java/com/example/bean/server/model/Product.java
+++ server/src/main/java/com/example/bean/server/model/Product.java
@@ -10,10 +10,21 @@ import lombok.Getter;
  * is intended to be used as a data model for products in an online store system where various types
  * of products need to be managed and stored.
  *
+ * <p>This class provides methods for creating, accessing, and updating product information.
+ * Additionally, it ensures that attributes are kept consistent with the following principles:
+ * <ul>
+ *   <li>{@code productID}, a unique identifier for the product, remains immutable once set.</li>
+ *   <li>{@code createdAt} and {@code updatedAt} timestamps are automatically updated whenever any
+ *       attribute of the product is modified.</li>
+ *   <li>{@code productName}, {@code category}, {@code price}, and {@code description} are mutable
+ *       and can be modified after the product is created.</li>
+ *   <li>{@code productName}, {@code category}, {@code price}, and {@code description} can be set
+ *       individually using dedicated setter methods, which ensure proper timestamp updates.</li>
+ * </ul>
+ *
  * @version 1.0
  * @since 2023-08-04
  */
-
 @Getter
 public class Product {
 
@@ -26,8 +37,8 @@ public class Product {
   private LocalDateTime updatedAt;
 
   /**
-   * Constructs a new product with the specified parameters, setting the createdAt and updatedAt
-   * timestamps to the current date and time.
+   * Constructs a new product with the specified parameters, setting the {@code createdAt} and
+   * {@code updatedAt} timestamps to the current date and time.
    *
    * @param productID   The unique identifier for the product.
    * @param productName The name of the product.
@@ -61,7 +72,7 @@ public class Product {
   }
 
   /**
-   * Sets the name of the product and updates the updatedAt timestamp.
+   * Sets the name of the product and updates the {@code updatedAt} timestamp.
    *
    * @param productName The new name of the product.
    */
@@ -71,7 +82,7 @@ public class Product {
   }
 
   /**
-   * Sets the category of the product and updates the updatedAt timestamp.
+   * Sets the category of the product and updates the {@code updatedAt} timestamp.
    *
    * @param category The new category of the product.
    */
@@ -81,7 +92,7 @@ public class Product {
   }
 
   /**
-   * Sets the price of the product and updates the updatedAt timestamp.
+   * Sets the price of the product and updates the {@code updatedAt} timestamp.
    *
    * @param price The new price of the product in the smallest unit of currency (e.g., cents).
    */
@@ -91,7 +102,7 @@ public class Product {
   }
 
   /**
-   * Sets the description of the product and updates the updatedAt timestamp.
+   * Sets the description of the product and updates the {@code updatedAt} timestamp.
    *
    * @param description The new description of the product.
    */
@@ -101,8 +112,8 @@ public class Product {
   }
 
   /**
-   * Updates the updatedAt timestamp to the current date and time. This method is called internally
-   * whenever any attribute of the product is modified.
+   * Updates the {@code updatedAt} timestamp to the current date and time. This method is called
+   * internally whenever any attribute of the product is modified.
    */
   private void update() {
     this.updatedAt = LocalDateTime.now();

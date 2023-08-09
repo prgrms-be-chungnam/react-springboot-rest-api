c31c331a4750b7fd05b119e85ab58bb144ea2614
Ji Myoung Ha <webmaster@null0xff.com>
2023-08-09 15:35:40 +0900

    :sparkles: created products page with controller

---
+++ server/src/main/java/com/example/bean/server/controller/ProductController.java
@@ -0,0 +1,24 @@
+package com.example.bean.server.controller;
+
+import com.example.bean.server.service.ProductService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ProductController {
+
+  private final ProductService productService;
+
+  public ProductController(ProductService productService) {
+    this.productService = productService;
+  }
+
+  @GetMapping("/products")
+  public String productsPage(Model model) {
+    var products = productService.getAllProducts();
+    model.addAttribute("products", products);
+    return "product-list";
+  }
+
+}

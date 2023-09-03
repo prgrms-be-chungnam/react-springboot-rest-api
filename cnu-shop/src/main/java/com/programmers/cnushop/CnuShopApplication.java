f392dd9b3c2c2dd51c8d7bcf01b3a5fa34e0a9ba
Hanjaemo <hjm2530@gmail.com>
2023-09-03 20:54:24 +0900

    init

---
+++ cnu-shop/src/main/java/com/programmers/cnushop/CnuShopApplication.java
@@ -0,0 +1,13 @@
+package com.programmers.cnushop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnuShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CnuShopApplication.class, args);
+    }
+
+}
---
+++ cnu-shop/src/test/java/com/programmers/cnushop/CnuShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programmers.cnushop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CnuShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

444c4caf7e515cad70dfdaab68d42ef9b5461e04
Song Ahyeon <ahyeonsong2001@gmail.com>
2023-09-13 18:13:45 +0900

    [feat] Spring 프로젝트 생성

---
+++ src/test/java/toyproject/interpark/InterparkApplicationTests.java
@@ -0,0 +1,13 @@
+package toyproject.interpark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterparkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

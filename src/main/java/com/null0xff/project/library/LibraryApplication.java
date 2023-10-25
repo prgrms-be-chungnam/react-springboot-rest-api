70f6e19e4160f85070d9810df654fa1e2e43a8a8
Ji Myoung Ha <webmaster@null0xff.com>
2023-10-25 15:24:09 +0900

    :tada: Gradle 프로젝트 생성 및 Dependencies 추가

---
+++ src/main/java/com/null0xff/project/library/LibraryApplication.java
@@ -0,0 +1,13 @@
+package com.null0xff.project.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LibraryApplication.class, args);
+  }
+
+}
---
+++ src/main/java/com/null0xff/project/library/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.null0xff.project.library;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(LibraryApplication.class);
+  }
+
+}
